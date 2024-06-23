7268a5e092541dc9d34c2d4034ed888d0c4e5d7d
JSH0905 <2000tjdgns@naver.com>
2024-06-24 01:53:02 +0900

    Form 요청/전송 객체 분리

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -23,18 +23,18 @@ public class Item {
      * 해결방법 : 스프링에서 BeanValidation의 groups -> 실무에서는 잘 안씀 -> 실무에서는 Form 객체 분리 방법을 사용
      */
 
-    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
+//    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min=1000, max=1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min=1000, max=1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
