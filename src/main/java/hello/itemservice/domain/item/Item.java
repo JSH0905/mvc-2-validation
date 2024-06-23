ec672cfdc53ec0db3c48c8690905f34ca9336f70
JSH0905 <2000tjdgns@naver.com>
2024-06-23 17:33:58 +0900

    Bean Validation - Object Error

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,19 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+/**
+ * 글로벌오류(Object Error)를 위해 @ScriptAssert()를 사용하는데 더이상 스프링에서 자바스크립트 런타임을 지원하지 않음
+ * 임시방편으로 build.gradle -> org.openjdk.nashorn:nashorn-core:15.3 의존성 추가
+ * 하지만 ScriptAssert 보다 기존에 했었던 순수 자바코드를 더 선호함.
+ */
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10000원 넘게 입력해주세요")
 public class Item {
 
     private Long id;
